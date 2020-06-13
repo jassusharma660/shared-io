d8681f52f93a052a378b5c5f30e7e6c5ce5ea851
Jassu Sharma <jassusharma660@gmail.com>
2020-06-13 19:32:06 +0530

    Added header and footer

---
+++ app/view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+  <span class="developer">Made with <img src="<?=ASSETS?>/images/icons/pixel-heart-50.png" alt="love"> by Jassu Sharma</span>
+  <span id="copyright"></span>
+</footer>
+<script>
+  document.getElementById("copyright").innerHTML = "&copy; "+new Date().getFullYear();
+</script>
